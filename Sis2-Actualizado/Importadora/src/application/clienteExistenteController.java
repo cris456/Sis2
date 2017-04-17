034e0274f98bbd4fbd698b0653a337379997e4df
cris456 <logicomito@gmail.com>
2017-04-16 21:38:57 -0400

    Actualizacion de Nombres
    Nombres Cambiados

---
+++ Sis2-Actualizado/Importadora/src/application/clienteExistenteController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class clienteExistenteController {
+        @FXML
+	private TextField campoNombrePrd;
+        @FXML
+	private TextField campoCantidad;
+        @FXML
+	private TextField campoCostoPedido;
+        @FXML
+	private TextField campoCostProducto;
+	@FXML
+	private TextField campoCiNit;
+	@FXML
+	private Button btnCancelar;
+        @FXML
+	private Button btnRegistrar;
+        
+        
+        
+        @FXML
+	private ComboBox<String> comboProveedores;
+	/*
+	ObservableList<String> Proveedores = FXCollections.observableArrayList("Entregado","SinObservaciones","Danhado","En Deposito");
+	
+	@FXML
+	private void inicializar(){
+		comboProveedores.setItems(Proveedores);
+	}*/
+	
+
+}
